7489eb4062c78d437891920404566c5a55d0b0bc
Pedroo <pedrohenriquemoalive@gmail.com>
2024-04-07 13:53:28 -0300

    estlizaçao do main e footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Search, Home as HomeIcon, Library } from "lucide-react";
+import { Search, Home as HomeIcon, Library, ChevronLeft, ChevronRight, Play, CirclePlus, Shuffle, SkipBack, CirclePause, SkipForward, Repeat, SquarePlay, ListMusic, Volume1, MonitorSmartphone, Maximize2 } from "lucide-react";
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
@@ -11,16 +12,16 @@ export default function Home() {
           </div>
           <nav className="space-y-5 mt-10">
             <a href="" className="flex items-center text-sm text-zinc-200 gap-3 font-semibold">
-              <HomeIcon/>
+              <HomeIcon />
               Home
 
             </a>
-            <a href=""className="flex items-center text-sm text-zinc-200 gap-3 font-semibold">
-              <Search/>
+            <a href="" className="flex items-center text-sm text-zinc-200 gap-3 font-semibold">
+              <Search />
               Search
             </a>
-            <a href=""className="flex items-center text-sm text-zinc-200 gap-3 font-semibold">
-              <Library/>
+            <a href="" className="flex items-center text-sm text-zinc-200 gap-3 font-semibold">
+              <Library />
               Library
             </a>
           </nav>
@@ -34,11 +35,122 @@ export default function Home() {
           </nav>
         </aside>
         <main className="flex-1 p-6">
-          main
+          <div className="flex items-center gap-3">
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="font-semibold text-3xl mt-8">Good Morning</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a className="bg-white/10 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 rounded overflow-hidden group  flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 rounded overflow-hidden group flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 rounded overflow-hidden group flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 rounded overflow-hidden group flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 rounded overflow-hidden group flex items-center gap-4 hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" width={80} height={80} />
+              <strong>Celebridade</strong>
+              <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+          <h2 className="font-semibold text-2xl mt-8">Made for Pedroo</h2>
+          <div className="grid grid-cols-5 mt-4 gap-4">
+            <a className="bg-white/5 p-2 rounded gap-2 flex flex-col hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" className="w-full" width={120} height={120} />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">Orochi,Kayblack, VEIGH ,VulgoFK and more</span>
+            </a><a className="bg-white/5 p-3 rounded gap-2 flex flex-col hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" className="w-full" width={120} height={120} />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">Orochi,Kayblack, VEIGH ,VulgoFK and more</span>
+            </a><a className="bg-white/5 p-2 rounded gap-2 flex flex-col hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" className="w-full" width={120} height={120} />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">Orochi,Kayblack, VEIGH ,VulgoFK and more</span>
+            </a><a className="bg-white/5 p-2 rounded gap-2 flex flex-col hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" className="w-full" width={120} height={120} />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">Orochi,Kayblack, VEIGH ,VulgoFK and more</span>
+            </a><a className="bg-white/5 p-2 rounded gap-2 flex flex-col hover:bg-white/10">
+              <Image src="/album.jpeg" alt="capa do album celebridade" className="w-full" width={120} height={120} />
+              <strong className="font-semibold">Daily mix 1</strong>
+              <span className="text-sm text-zinc-400">Orochi,Kayblack, VEIGH ,VulgoFK and more</span>
+            </a>
+          </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+      <footer className="bg-black border-none p-6 fixed bottom-0 w-full">
+        <div className="flex justify-between">
+          <div className="flex gap-3 items-center">
+            <Image src="/album.jpeg" alt="capa do album celebridade" className="rounded-t-sm" width={60} height={60} />
+            <div className="flex flex-col">
+              <strong className="font-semibold hover:underline">Celebridade</strong>
+              <span className="text-zinc-400 text-sm ">Orochi</span>
+            </div>
+            <CirclePlus className="size-5 text-zinc-400" />
+          </div>
+          <div className="flex flex-col items-center gap-1 justify-center">
+            <div className="flex gap-4 text-zinc-400">
+              <Shuffle className="text-green-600" />
+              <SkipBack />
+              <CirclePause className="text-zinc-200"/>
+              <SkipForward />
+              <Repeat/>
+            </div>
+            <div className="flex items-center gap-1">
+              <span className="text-zinc-500 text-sm">0:31</span>
+              <div className="rounded-full h-1 bg-zinc-600 w-96">
+                <div className="rounded-full h-1 w-28 bg-zinc-200"></div>
+              </div>
+              <span className="text-zinc-500 text-sm">2:38</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 text-zinc-400">
+            <SquarePlay/>
+            <ListMusic/>
+            <Volume1/>
+            <div className="rounded-full h-1 w-24 bg-zinc-600">
+              <div className="rounden-full h-1 w-8 bg-zinc-200"></div>
+            </div>
+            <MonitorSmartphone/>
+            <Maximize2/>
+          </div>
+        </div>
       </footer>
     </div>
   );

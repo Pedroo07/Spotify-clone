f42eece19f0e9f0f020f17e960623c8f33df14ce
Pedroo <pedrohenriquemoalive@gmail.com>
2024-04-09 12:34:23 -0300

    configurando basepath

--- src/app/components/MusicAlbum.tsx
+++ src/app/components/MusicAlbum.tsx
@@ -6,7 +6,7 @@ import { ItemProps } from '../types'
 export const MusicAlbum: React.FC<ItemProps> = ({ src, children }) => {
   return (
     <a className="bg-white/10 rounded group overflow-hidden flex items-center gap-4 hover:bg-white/10">
-      <Image src={src ?? ''} alt="capa do album do orochi" width={80} height={80} />
+      <Image src={src ? '/Spotify-clone'+ src : ''} alt="capa do album do orochi" width={80} height={80} />
       <strong>{children}</strong>
       <button className="flex items-center justify-center ml-auto mr-8 h-10 w-10 rounded-full bg-green-500 text-black pl-1 invisible group-hover:visible">
         <Play />
